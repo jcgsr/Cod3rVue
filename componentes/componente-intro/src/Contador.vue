7694d05f2bbb61b3c9124719c1632c9d93e18007
jcgsr <cabralguerra@gmail.com>
2020-06-24 21:36:09 -0300

    componentes, cli

---
+++ componentes/componente-intro/src/Contador.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="contador">
+    <span>{{ contador }}</span>
+    <button @click="adicionar" class="btn btn-success">+</button>
+    <button @click="subtrair" class="btn btn-danger">-</button>
+  </div>
+</template>
+
+<script>
+export default{
+  data() {
+    return {
+      contador: 0
+    }
+  },
+  methods: {
+    adicionar() {
+      this.contador++
+    },
+    subtrair() {
+      this.contador--
+    }
+  }
+}
+</script>
+
+<style>
+.btn{
+  margin-left: 1rem;
+}
+</style>
