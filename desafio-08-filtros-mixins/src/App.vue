07bd7c022cf3fb4b5f4d74d99a24af074162cdfe
jcgsr <cabralguerra@gmail.com>
2020-07-12 18:45:36 -0300

    desafio filter e mixin

--- desafio-08-filtros-mixins/src/App.vue
+++ desafio-08-filtros-mixins/src/App.vue
@@ -3,14 +3,19 @@
 		<h1>Filtros & Mixins (Desafio)</h1>
 		<!-- Exercício 1 -->
 		<!-- Construir um filtro local que troca espaços por vírgula -->
-		
+		<p>{{ frase | espacoPorVirgula}}</p>
+
 		<!-- Exercício 2 -->
 		<!-- Filtro global que conta o tamanho de cada palavra e adiciona o 
 			valor na string final -->
 		<!-- "Pedro é legal" => "Pedro (5) é (1) legal (5)" -->
+		<p>{{ frase | contarPalavra }}</p>
+
 
 		<!-- Exercício 3 -->
 		<!-- Implementar os exercicios 1 e 2 com propriedade computada -->
+		<p>{{ fraseComVirgulas }}</p>
+		<p>{{ fraseComTamanhos }}</p>
 
 		<!-- Exercício 4 -->
 		<!-- Compartilhe a propriedade computada via mixin -->
@@ -18,8 +23,20 @@
 </template>
 
 <script>
+import frasesMixin from './fraseMixin'
+
 export default {
-	
+	mixins: [frasesMixin], 
+	data() {
+		return {
+			frase: 'Painho faz muita falta! Que saudade...'
+		}
+	},
+	filters: {
+		espacoPorVirgula(valor) {
+			return valor.replace(/\s/g, ',')
+		}
+	}	
 }
 </script>
 
--- desafio-08-filtros-mixins/src/main.js
+++ desafio-08-filtros-mixins/src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// Filtro Global
+Vue.filter('contarPalavra', function(valor) {
+	return valor.split(' ').map(p => `${p} (${p.length})`).join(' ')
+})
+
 new Vue({
 	render: h => h(App),
 }).$mount('#app')
