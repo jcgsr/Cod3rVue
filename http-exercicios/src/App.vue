65608069f8a3ad4d6f5b254206d27b58a2e279e9
jcgsr <cabralguerra@gmail.com>
2020-07-13 10:14:06 -0300

    axios get

--- http-exercicios/src/App.vue
+++ http-exercicios/src/App.vue
@@ -11,7 +11,16 @@
       </b-form-group>
       <hr />
       <b-button @click="salvar" size="lg" variant="primary">Salvar</b-button>
+      <b-button @click="obterUsuarios" size="lg" variant="success" class="ml-2">Obter Usuários</b-button>
     </b-card>
+    <hr />
+    <b-list-group>
+		 <b-list-group-item v-for="(usuario, id) in usuarios" :key="id">
+			 <strong>Nome: </strong>{{ usuario.nome }} <br />
+			 <strong>E-mail: </strong>{{ usuario.email }} <br />
+			 <strong>ID: </strong> {{ id }}
+		 </b-list-group-item>
+	 </b-list-group>
   </div>
 </template>
 
@@ -19,6 +28,7 @@
 export default {
   data() {
     return {
+      usuarios: [],
       usuario: {
         nome: "",
         email: ""
@@ -31,6 +41,11 @@ export default {
         this.usuario.nome = "";
         this.usuario.email = "";
         alert("Cadastro efetuado!");
+      });
+    },
+    obterUsuarios() {
+      this.$http.get("usuarios.json").then(res => {
+        this.usuarios = res.data;
       });
     }
   }
