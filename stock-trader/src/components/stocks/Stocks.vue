fa056917f14555f57a088569440c5980717dd8dd
jcgsr <cabralguerra@gmail.com>
2020-07-23 16:46:29 -0300

    almost there

--- stock-trader/src/components/stocks/Stocks.vue
+++ stock-trader/src/components/stocks/Stocks.vue
@@ -5,19 +5,14 @@
 </template>
 
 <script>
-import Stock from './Stock'
+import Stock from "./Stock";
 
 export default {
   components: { Stock },
-  data() {
-    return {
-      stocks: [
-        { id: 1, name: 'BMW', price: 110},
-        { id: 2, name: 'Google', price: 200},
-        { id: 3, name: 'Apple', price: 280},
-        { id: 4, name: 'Twitter', price: 83}        
-      ]
-    }
-  }, 
-}
+  computed: {
+    stocks() {
+      return this.$store.getters.stocks
+    },
+  },
+};
 </script>
